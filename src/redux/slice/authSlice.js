37fe7d13a7deabfda8ec32b92afe9e1c8ec34dbb
vipul waghmare <Vipul.Waghmare@OrciCare.com>
2023-02-25 15:30:41 +0530

    authentication added

--- src/redux/slice/authSlice.js
+++ src/redux/slice/authSlice.js
@@ -1,34 +1,35 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { getAuthStatusAction, signinAction } from "../thunks/authThunk";
 import Cookies from "js-cookie";
-import { toast } from 'react-toastify';
-import { t } from "src/locales/localeHelper"
+import { toast } from "react-toastify";
+import { t } from "src/locales/localeHelper";
+import { auth } from "src/config/firebaseConfig";
 
 const initialState = {
   loading: false,
   isAuthTokenChecked: false,
-  isAuth: true,
+  isAuth: false,
   authUserInfo: {
     // email: "",
     jwtToken: "",
     jwtTokenDecode: "",
   },
+  userFirebaseInfo: null,
 };
-
 
 const authSlice = createSlice({
   name: "authSlice",
   initialState,
   reducers: {
     signOutAction: (state) => {
-      state.isAuth = false;
-      state.authUserInfo = initialState.authUserInfo;
-      try {
-        Cookies.remove("jwtToken")
-        Cookies.remove("refreshToken")
-      } catch (error) {
-
-      }
+      auth.signOut();
+      state = initialState;
+      // state.isAuth = false;
+      // state.authUserInfo = initialState.authUserInfo;
+      // try {
+      //   Cookies.remove("jwtToken");
+      //   Cookies.remove("refreshToken");
+      // } catch (error) {}
     },
   },
 
@@ -38,14 +39,15 @@ const authSlice = createSlice({
     },
     [getAuthStatusAction.fulfilled]: (state, action) => {
       if (action.payload) {
-        const { jwtToken, jwtTokenDecode, } = action.payload
+        const { jwtToken, jwtTokenDecode } = action.payload;
         state.isAuth = true;
         state.isAuthTokenChecked = true;
         state.authUserInfo.jwtToken = jwtToken;
         state.authUserInfo.jwtTokenDecode = jwtTokenDecode;
+        state.userFirebaseInfo = action.payload;
       } else {
         // state.isAuth = false;
-        state.authUserInfo = initialState.authUserInfo
+        state.authUserInfo = initialState.authUserInfo;
       }
       state.isLoading = false;
     },
@@ -56,21 +58,20 @@ const authSlice = createSlice({
       state.authUserInfo = initialState.authUserInfo;
     },
 
-
     [signinAction.pending]: (state, action) => {
       state.loading = true;
     },
     [signinAction.fulfilled]: (state, action) => {
       state.loading = false;
       if (action.payload) {
-        const { jwtToken, jwtTokenDecode } = action.payload
+        const { jwtToken, jwtTokenDecode } = action.payload;
         state.isAuth = true;
         state.authUserInfo.jwtToken = jwtToken;
         state.authUserInfo.jwtTokenDecode = jwtTokenDecode;
-        toast.success(t("nofn-msg-to-login"), "", 2000)
+        toast.success(t("nofn-msg-to-login"), "", 2000);
       } else {
         // state.isAuth = false;
-        state.authUserInfo = initialState.authUserInfo
+        state.authUserInfo = initialState.authUserInfo;
       }
     },
     [signinAction.rejected]: (state, action) => {
@@ -82,6 +83,6 @@ const authSlice = createSlice({
   },
 });
 
-export const { signOutAction } = authSlice.actions
+export const { signOutAction } = authSlice.actions;
 
 export default authSlice;
--- src/redux/thunks/authThunk.js
+++ src/redux/thunks/authThunk.js
@@ -1,93 +1,154 @@
-import { createAsyncThunk } from '@reduxjs/toolkit'
+import { createAsyncThunk } from "@reduxjs/toolkit";
 import { getAxios } from "src/helpers/axiosInterceptor";
 import Cookies from "js-cookie";
 import jwt_decode from "jwt-decode";
-import { DeviceUUID } from 'device-uuid';
-import { t } from 'src/locales/localeHelper';
-import { toast } from 'react-toastify';
+import { DeviceUUID } from "device-uuid";
+import { t } from "src/locales/localeHelper";
+import { toast } from "react-toastify";
+import { auth } from "src/config/firebaseConfig";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+
+const firebaseAuthPromise = new Promise((res, rej) => {
+  onAuthStateChanged(auth, (currentUser) => {
+    // console.log(currentUser);
+    if (currentUser) {
+      res(currentUser);
+    } else {
+      rej("user not found");
+    }
+  });
+});
 
 export const getAuthStatusAction = createAsyncThunk(
   "auth/getAuthStatusAction",
   async (arg, thunkApi) => {
     try {
+      return firebaseAuthPromise;
+      // const payload = {
+      //   jwtToken: "",
+      //   jwtTokenDecode: {},
+      //   isAuthTokenChecked: true,
+      // };
 
-      const payload = {
-        jwtToken: "",
-        jwtTokenDecode: {},
-        isAuthTokenChecked: true,
-      }
+      // let jwtToken = Cookies.get("jwtToken");
+      // if (!jwtToken) {
+      //   const responseData = await thunkApi
+      //     .dispatch(getTokenFromRefreshToken())
+      //     .unwrap();
+      //   jwtToken = responseData.jwtToken;
+      // }
 
-      let jwtToken = Cookies.get('jwtToken')
-      if (!jwtToken) {
-        const responseData = await thunkApi.dispatch(getTokenFromRefreshToken()).unwrap()
-        jwtToken = responseData.jwtToken
-      }
+      // if (jwtToken) {
+      //   const jwtTokenDecode = jwt_decode(jwtToken);
+      //   payload.jwtToken = jwtToken;
+      //   payload.jwtTokenDecode = jwtTokenDecode;
+      // }
 
-      if (jwtToken) {
-        const jwtTokenDecode = jwt_decode(jwtToken);
-        payload.jwtToken = jwtToken;
-        payload.jwtTokenDecode = jwtTokenDecode
-      }
-
-      return payload;
+      // return payload;
     } catch (error) {
       return thunkApi.rejectWithValue(error);
     }
-  });
-
+  },
+);
 
 export const getTokenFromRefreshToken = createAsyncThunk(
   "auth/getTokenFromRefreshToken",
   async (arg, thunkApi) => {
     try {
-
-      const refreshToken = Cookies.get("refreshToken")
+      const refreshToken = Cookies.get("refreshToken");
       if (refreshToken) {
         const uuid = new DeviceUUID().get();
-        const response = await getAxios().post(
-          `/iam/user/refreshtoken`,
-          {
-            deviceId: uuid,
-            refreshToken: refreshToken,
-          }
-        );
+        const response = await getAxios().post(`/iam/user/refreshtoken`, {
+          deviceId: uuid,
+          refreshToken: refreshToken,
+        });
 
-        return {  jwtToken: response.token }
+        return { jwtToken: response.token };
       }
       return null;
     } catch (err) {
       return thunkApi.rejectWithValue(err);
     }
-  }
+  },
 );
+export const signupAction = createAsyncThunk(
+  "auth/signupAction",
+  async (arg, thunkApi) => {
+    try {
+      // const uuid = new DeviceUUID().get();
 
+      // const requestModel = {
+      //   email: arg.username,
+      //   password: arg.password,
+      //   rememberme: arg.isKeepLoggedIn,
+      //   deviceId: uuid,
+      // };
+
+      const user = await createUserWithEmailAndPassword(
+        auth,
+        arg.username,
+        arg.password,
+      );
+      return user;
+      // console.log({ user });
+
+      // const response = await getAxios().post("iam/user/token", requestModel);
+      // const { token, refreshToken } = response.data;
+
+      // const jwtTokenDecode = jwt_decode(token);
+      // Cookies.set("jwtToken", token, { expires: 7 });
+
+      // if (refreshToken) {
+      //   // const refreshTokenDecode = jwt_decode(refreshToken)
+      //   Cookies.set("refreshToken", refreshToken, { expires: 90 });
+      // }
+
+      // return { jwtToken: token, jwtTokenDecode, isAuthTokenChecked: true };
+    } catch (error) {
+      return thunkApi.rejectWithValue(error);
+    }
+  },
+);
 
 export const signinAction = createAsyncThunk(
   "auth/signinAction",
   async (arg, thunkApi) => {
     try {
-      const uuid = new DeviceUUID().get();
+      // const uuid = new DeviceUUID().get();
 
-      const requestModel = {
-        email: arg.username,
-        password: arg.password,
-        rememberme: arg.isKeepLoggedIn,
-        deviceId: uuid,
-      }
+      // const requestModel = {
+      //   email: arg.username,
+      //   password: arg.password,
+      //   rememberme: arg.isKeepLoggedIn,
+      //   deviceId: uuid,
+      // };
 
-      const response = await getAxios().post("iam/user/token", requestModel);
-      const { token, refreshToken } = response.data
+      const user = await signInWithEmailAndPassword(
+        auth,
+        arg.username,
+        arg.password,
+      );
+      return user;
+      // console.log({ user });
 
-      const jwtTokenDecode = jwt_decode(token);
-      Cookies.set('jwtToken', token, { expires: 7 });
+      // const response = await getAxios().post("iam/user/token", requestModel);
+      // const { token, refreshToken } = response.data;
 
-      if (refreshToken) {
-        // const refreshTokenDecode = jwt_decode(refreshToken)
-        Cookies.set('refreshToken', refreshToken, { expires: 90 });
-      }
+      // const jwtTokenDecode = jwt_decode(token);
+      // Cookies.set("jwtToken", token, { expires: 7 });
 
-      return { jwtToken: token, jwtTokenDecode, isAuthTokenChecked: true }
+      // if (refreshToken) {
+      //   // const refreshTokenDecode = jwt_decode(refreshToken)
+      //   Cookies.set("refreshToken", refreshToken, { expires: 90 });
+      // }
+
+      // return { jwtToken: token, jwtTokenDecode, isAuthTokenChecked: true };
     } catch (error) {
       return thunkApi.rejectWithValue(error);
     }
-  });
+  },
+);
